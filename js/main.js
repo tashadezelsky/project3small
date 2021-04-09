70c129c7c46c4e8c810d5abc34e632b78ee061d1
tashadezelsky <77847692+tashadezelsky@users.noreply.github.com>
2021-04-09 20:50:57 +0900

    adding files

---
+++ js/main.js
@@ -0,0 +1,77 @@
+console.log("Please work");
+
+var Airtable = require("airtable");
+console.log(Airtable);
+
+var base = new Airtable({ apiKey: 'keyHlLjNPepQaUXmp'}).base('appaAFtNuU8ZmHaV8');
+
+base('dynamic').select({}).eachPage(gotPageOfMermaids, gotAllMermaids);
+    // This function (`page`) will get called for each page of records.
+
+const mermaids = [];
+
+function gotPageOfMermaids(records, fetchNextPage) {
+    console.log("gotPageOfMermaids()");
+    mermaids.push(...records);
+    fetchNextPage();
+}
+
+function gotAllMermaids(err) {
+    console.log("gotAllMermaids()");
+
+    if (err) {
+        console.log("error loading faces");
+        console.error(err);
+        return;
+       }
+
+consoleLogMermaids();
+showMermaids();
+}
+
+function consoleLogMermaids() {
+    console.log("consoleLogMermaids()");
+    mermaids.forEach((mermaid) => {
+    console.log("Mermaid:", mermaid);
+});
+}
+
+function showMermaids() {
+    console.log("showMermaids()");
+    mermaids.forEach((mermaid) => {
+
+        //var pictureTitle = document.createElement("h3");
+        //pictureTitle.innerText = mermaid.fields.title;
+        //document.body.append(pictureTitle);
+
+        //new div where the container for the images will go
+        var galleryContainer = document.createElement("div");
+        galleryContainer.classList.add("gallery-container");
+        document.querySelector(".container").append(galleryContainer);
+
+        var imageDesc = document.createElement("h4");
+        imageDesc.classList.add=("desc");
+        imageDesc.innerText = mermaid.fields.description;
+        galleryContainer.append(imageDesc);
+
+        var imageTitle = document.createElement("h1");
+        imageTitle.classList.add=("image-title");
+        imageTitle.innerText = mermaid.fields.title;
+        galleryContainer.append(imageTitle);
+
+        //add the picture 
+        //var galleryImage = document.createElement("img");
+        //galleryImage.classList.add("image-container");
+        //galleryImage.src = mermaid.fields.cursor[0].url;
+        //galleryContainer.append(galleryImage);
+
+        //var imageCursor = document.createElement("img");
+        //imageCursor.classList.add("image-cursor");
+        //document.querySelector(".cursor1").append(imageCursor);
+
+        //var imageBg = document.createElement("img");
+        //imageBg.classList.add("bgimage-container");
+        //imageBg.classList.src = mermaid.fields.cursor[0].url;
+        //imageCursor.append(imageBG);
+    })
+};
